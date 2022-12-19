0e452ca1cc23f472163561510a3f8fc8aa7ee26c
guruprasadesambattu <guruprasad.esambattu@mutualmobile.com>
2022-12-19 10:27:12 +0530

    MainActivity add HomeScreen() function

--- app/src/main/java/com/example/meditationui/MainActivity.kt
+++ app/src/main/java/com/example/meditationui/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.meditationui.ui.HomeScreen
 import com.example.meditationui.ui.theme.MeditationUITheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +18,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MeditationUITheme {
-
+                HomeScreen()
             }
         }
     }
